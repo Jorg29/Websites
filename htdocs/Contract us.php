f437c98303344f1197393de00e87d8c0fb3dab02
Jörg Kefalas <93915145+Jorg29@users.noreply.github.com>
2022-11-29 16:07:50 +0200

    Add files via upload

---
+++ htdocs/Contract us.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Arts</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   height: 15%;
+   background-color: black;
+   color: white;
+   text-align: center;
+}
+</style>
+</head> 
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"><img src="images/1.png" alt="1">  </a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="Home.php">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Arts <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="About us.php">About us</a></li>
+          <li><a href="Contract us.php">Contract us</a></li>
+          <li><a href="More info.php">More info</a></li>
+        </ul>
+      </li>
+      <li><a href="Gallery.php">Gallery</a></li>
+      <li><a href="Shop.php">Shop</a></li>
+    </ul>
+  </div>
+</nav>
+ 
+ <div class="container">
+  <h2>Contract</h2>
+  <ul class="list-group">
+    <li class="list-group-item">www.Artscustomerservices.com</li>
+    <li class="list-group-item">+2651064732
+</li>
+    <li class="list-group-item">Arts@gmail.com
+</li>
+  </ul>
+</div>
+
+<div class="footer">
+  <p><a href="About us.php">About us</a></p>
+<p><h4>Communication<h4></p>
+<p>+2651064732|Arts@gmail.com</p>
+<p><h6>Copyright © www.Arts.com 20020-2021<h6></p>
+<p><a href="images/1.png" target="_blank">
+  <img src="images/1.png" alt="1"</p>
+</div>
+
+</body>
+</html>
---
+++ htdocs/Gallery.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Arts</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   height: 15%;
+   background-color: black;
+   color: white;
+   text-align: center;
+}
+</style>
+</head> 
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"><img src="images/1.png" alt="1">  </a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="Home.php">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Arts <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="About us.php">About us</a></li>
+          <li><a href="Contract us.php">Contract us</a></li>
+          <li><a href="More info.php">More info</a></li>
+        </ul>
+      </li>
+      <li><a href="Gallery.php">Gallery</a></li>
+      <li><a href="Shop.php">Shop</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+  <h1>Welcome to Gallery</h1>
+
+</div>
+
+<div class="container">
+  <h2>Paintings</h2>  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="images/3.jfif" alt="3" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="images/8.jfif" alt="8" style="width:100%;"
+        >
+      </div>
+    
+      <div class="item">
+        <img src="images/5.jfif" alt="5" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<div class="footer">
+ <p><a href="About us.php">About us</a></p>
+<p><h4>Communication<h4></p>
+<p>+2651064732|Arts@gmail.com</p>
+<p><h6>Copyright © www.Arts.com 20020-2021<h6></p>
+<p><a href="images/1.png" target="_blank">
+  <img src="images/1.png" alt="1"</p>
+</div>
+
+</body>
+</html>
+
